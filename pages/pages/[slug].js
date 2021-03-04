9d8d192fd5afd74c6542e333993092b32fdfdd4b
Cefalu Wave <luke.evan.graham@gmail.com>
2021-03-03 22:58:45 -0800

    pages starting to work

---
+++ pages/pages/[slug].js
@@ -0,0 +1,47 @@
+import { getAllPageSlugs, getPageData, fetchAPI } from "../../lib/api";
+import Sections from "../../components/sections/sections"
+
+export async function getStaticPaths() {
+  const paths = await getAllPageSlugs();
+  console.log("paths: ", paths);
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const pageData = await getPageData(params.slug);
+  // const global = await fetchAPI("/global");
+  return {
+    props: {
+      pageData,
+      // global
+    },
+    revalidate: 1,
+  };
+}
+
+export default function Page({ pageData }) {
+
+    // Making sure we don't render the hero (first section)
+  let renderSections = pageData.contentSections ? (
+    <Sections
+      sections={pageData.contentSections}
+      preview={null}
+    />
+  ) : (
+    <div>Loading...</div>
+  );
+
+
+  return (
+    <>
+    {console.log("data: ", pageData)}
+      <section>
+        <div className="row">{pageData.shortName}</div>
+      </section>
+      {renderSections}
+    </>
+  );
+}
---
+++ pages/pages/index.js
@@ -0,0 +1,30 @@
+import { getSortedPageData, fetchAPI } from "../../lib/api";
+
+export async function getStaticProps() {
+  const allPageData = await getSortedPageData();
+  // const global = await fetchAPI("/global");
+
+  return {
+    props: {
+      allPageData,
+      // global
+    },
+    revalidate: 1,
+  };
+}
+
+const Pages = ({ allPageData, global }) => (
+  <>
+    <section>
+      <div className="row">
+        <div>All Pages</div>
+        {console.log("data: ", allPageData)}
+        {allPageData.map((page) => (
+          <div>{page.shortName}</div>
+        ))}
+      </div>
+    </section>
+  </>
+);
+
+export default Pages;
