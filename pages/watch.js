db188ed8130660cd8c9251f65f8406463be49f53
Cefalu Wave <luke.evan.graham@gmail.com>
2021-03-03 16:03:36 -0800

    working on the watch page

---
+++ pages/watch.js
@@ -0,0 +1,22 @@
+const Watch = (props) => (
+  <>
+    <section style={{ textAlign: "center" }}>
+      <h2>Watch</h2>
+      <div className="row">
+        <div className="col span-2-of-2">
+          <div style={{ marginBottom: "2.5rem" }}>Here's some text</div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col span-1-of-2">
+          <h3>Sermons</h3>
+        </div>
+        <div className="col span-1-of-2">
+          <h3>Services</h3>
+        </div>
+      </div>
+    </section>
+  </>
+);
+
+export default Watch;
