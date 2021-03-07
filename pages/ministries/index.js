af36ab7b780dff02483c438437825953571fa2cb
Cefalu Wave <luke.evan.graham@gmail.com>
2021-03-06 23:02:54 -0800

    better spacing on ministries list

--- pages/ministries/index.js
+++ pages/ministries/index.js
@@ -1,5 +1,8 @@
 import { getSortedMinistriesData, fetchAPI } from "../../lib/api";
 import Link from "next/link";
+import classNames from "classnames";
+
+import classes from "./index.module.css";
 
 export async function getStaticProps() {
   const allMinistriesData = await getSortedMinistriesData();
@@ -16,9 +19,9 @@ const Ministries = ({ allMinistriesData }) => {
   return (
     <section>
       <h2>Ministries</h2>
-      <div className="row">
+      <div className={`row ${classes.ministriesContainer}`}>
         {allMinistriesData.map((ministry) => (
-          <div key={ministry.id}>
+          <div key={ministry.id} className={classes.ministry}>
             <Link href={`ministries/${ministry.slug}`}>
               <a>{ministry.ministryName}</a>
             </Link>
